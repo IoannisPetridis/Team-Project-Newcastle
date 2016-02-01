fc9bbdc87ba3f01403ee06718ef8b268cd734e68
irokin <gu.irokin@gmail.com>
2016-02-01 23:36:06 +0000

    Update

--- Test Project/Test Project/Source.cpp
+++ Test Project/Test Project/Source.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
+class enemy {
+	enemy() = default;
+};
+
+struct test {
+	enemy* e1;
+};
+
 int main() {
 	using std::cout;
 	using std::endl;
 
 	cout << "This is Yulong" << endl;
 	cout << "Testing Testing" << endl;
-	
+
 	return 0;
 }
