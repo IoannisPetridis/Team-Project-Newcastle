90b225fcf90724eb0d9b3216272246a63e388514
irokin <irokin@users.noreply.github.com>
2016-02-01 14:58:52 +0000

    Update test.cpp

--- Ioannis/test.cpp
+++ Ioannis/test.cpp
@@ -1,15 +1,11 @@
 #include <iostream.h>
 
 int main() {
-
-<<<<<<< HEAD
 using namespace std;
 
 cout << "Yulong Gu, Eric" << endl;
 cout << "tset" << endl;
 
-=======
->>>>>>> refs/remotes/origin/master
 return 0;
 
 }
