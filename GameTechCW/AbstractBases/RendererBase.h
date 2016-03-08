9cfdf1919f2f1fb4f00b64b4028406b358f0a52a
irokin <gu.irokin@gmail.com>
2016-03-08 12:18:50 +0000

    Physics Stuff

---
+++ GameTechCW/AbstractBases/RendererBase.h
@@ -0,0 +1,12 @@
+#pragma once
+class RendererBase
+{
+public:
+	RendererBase();
+	virtual ~RendererBase();
+
+	virtual void UpdateScene(float dt)	= 0;
+	virtual void RenderScene()			= 0;
+	virtual void SwapBuffers()			= 0;
+};
+
